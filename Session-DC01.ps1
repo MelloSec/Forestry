50854384f37cc21081721c114b910764bc90fd64
mellonaut <65114647+mellonaut@users.noreply.github.com>
2022-08-23 16:24:44 -0400

    Testing

--- Session-DC01.ps1
+++ Session-DC01.ps1
@@ -1,3 +1,3 @@
 # set-item wsman:\localhost\Client\TrustedHosts -Value *
-$DC1 = New-PSSession -ComputerName 192.168.0.83 -Credential (get-Credential)
+$DC1 = New-PSSession -ComputerName 192.168.0.83 -Credential Administrator
 Enter-PsSession $DC1
