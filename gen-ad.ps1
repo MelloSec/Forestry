2de4dc3c46745ba75f63cb98ac88d50c441a16ba
mellonaut <65114647+mellonaut@users.noreply.github.com>
2022-08-23 16:05:52 -0400

    Main script for processing the json file is complete

--- gen-ad.ps1
+++ gen-ad.ps1
@@ -2,10 +2,15 @@ param(
     [Parameter(Mandatory=$true)]$JSONFile
 )
 
-function Create-ADUSer {
-    param( [Parameter(Mandatory=$true)]$userObject )
-      
-    # Name from JSon object, then create first name last name structure
+function CreateADGroup(){
+    $name = $groupObject.Name
+    New-ADGroup -name $name -GroupScope Global
+
+}
+
+function Create-ADUSer(){
+    param( [Parameter(Mandatory=$true)]$userObject )  
+      # Name from JSON object, then create first name last name structure
       $name = $userObject.name
       $password = $userObject.password
       $firstname, $lastname = $name.split(" ")
@@ -13,11 +18,21 @@ function Create-ADUSer {
       $samAccountName = $username
       $principalName = $username
 
-    New-ADUser -Name "$firstname $lastname" -GivenName $firstname -Surname $lastname -SamAccountName $samAccountName -UserPrincipalName $principalname@$Global:Domain -AccountPassword (ConvertTo-SecureString $generated_password -AsPlainText -Force) -PassThru | Enable-ADAccount 
+      # Create the actual AD User Object 
+      New-ADUser -Name "$name" -GivenName $firstname -Surname $lastname -SamAccountName $samAccountName -UserPrincipalName $principalname@$Global:Domain -AccountPassword (ConvertTo-SecureString $password -AsPlainText -Force) -PassThru | Enable-ADAccount 
+  
+      # Add user to approproate groups
+      foreach($group_name in $userObject.groups){
+        try {
+            Get-ADGroup -Identity "$group_name"
+            Add-ADGroupMember -Identity $group_name -Members $username
+        }
+        catch [Microsoft.ActiveDirectory.Management.ADIdentityNotFoundException] {
+            Write-Warning "$username not added to group $group_name because it does not exist"
+        }
 
-   
-    echo $UserObject
-
+      Add-ADGroupMember -Identity $group -Member $username
+    }
 }
 
 # $json = ( Get-Content $JSONFile | ConvertFrom-Json )
@@ -25,9 +40,9 @@ $json = $JSONFile | ConvertFrom-Json
 
 $Global:Domain = $json.domain
 
-
-
-
+foreach ( $group in $json.groups ){
+    Create-ADGroup $group
+}
 
 foreach ( $user in $json.users ){
     Create-ADUser -UserObject $user
