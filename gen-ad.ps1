287876471fd856ebfab2798d03baf1be617fa209
mellonaut <65114647+mellonaut@users.noreply.github.com>
2022-08-23 15:41:19 -0400

    New functions for automating the schema and user setup with JSON

--- gen-ad.ps1
+++ gen-ad.ps1
@@ -3,15 +3,32 @@ param(
 )
 
 function Create-ADUSer {
-    param( [Parameter(Mandatory=$true)]$UserObject )
+    param( [Parameter(Mandatory=$true)]$userObject )
+      
+    # Name from JSon object, then create first name last name structure
+      $name = $userObject.name
+      $password = $userObject.password
+      $firstname, $lastname = $name.split(" ")
+      $username = ($firstname[0] + $lastname).ToLower()
+      $samAccountName = $username
+      $principalName = $username
+
+    New-ADUser -Name "$firstname $lastname" -GivenName $firstname -Surname $lastname -SamAccountName $samAccountName -UserPrincipalName $principalname@$Global:Domain -AccountPassword (ConvertTo-SecureString $generated_password -AsPlainText -Force) -PassThru | Enable-ADAccount 
+
+   
     echo $UserObject
 
-
-    # $User = New-Object -TypeName 'System.Security.Principal.WindowsPrincipal' -ArgumentList @($JSONFile)
-    # $User.GetUnderlyingObject()
 }
 
-$json = ( Get-Content $JSONFile | ConvertFrom-Json )
+# $json = ( Get-Content $JSONFile | ConvertFrom-Json )
+$json = $JSONFile | ConvertFrom-Json
+
+$Global:Domain = $json.domain
+
+
+
+
+
 foreach ( $user in $json.users ){
     Create-ADUser -UserObject $user
 }
