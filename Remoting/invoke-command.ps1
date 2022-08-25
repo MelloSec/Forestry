8fec810f52822ca82e25751a4529a574e68f0f06
mellonaut <65114647+mellonaut@users.noreply.github.com>
2022-08-25 16:54:45 -0400

    Invoke-Command script and Remote Runner all working well. Invoke command is a good test example for demoing PSremote

--- Remoting/invoke-command.ps1
+++ Remoting/invoke-command.ps1
@@ -1,1 +1,17 @@
-Invoke-Command -ComputerName $Computer -Credential $cred -ScriptBlock 
+$Computer = Get-Content hosts.csv
+if ($cred)
+    { Write-Host "Variable already set" } else { $cred = (Get-Credential) }
+
+$sessions = New-PSSession -ComputerName $Computer -Credential $cred
+Invoke-Command -ComputerName $Computer -Credential $cred -ScriptBlock { hostname; net user; ipconfig; Echo "`r`n";Echo "`r`n";Echo "`r`n";Echo "`r`n";Echo "Next Host:";Echo "`r`n";Echo "`r`n" } 
+
+
+#     function Invoke-Sessions {
+#         param(
+#             [Parameter(Mandatory=$true)]
+#             [String]$cmd
+#         )
+# Invoke-Command -ComputerName $Computer -Credential $cred -ScriptBlock { $cmd }
+# }
+
+# Invoke-Sessions
